4eb3c33de6999d6d51098a1a247bab9020c7084e
Seph Reed <ShadyWillowCreek@gmail.com>
2016-06-30 17:28:26 -0500

    I should sync more often

---
+++ digiden.js
@@ -0,0 +1,23 @@
+var LIGHT_MODE_ENABLED;
+
+function setLightMode(onOff) {
+    if(onOff !== LIGHT_MODE_ENABLED) {
+        // if(LIGHT_MODE_ENABLED != undefined)
+        //     document.body.style.transitionDuration = "0.5s";
+            
+        LIGHT_MODE_ENABLED = onOff;
+        localStorage.setItem("LIGHT_MODE", LIGHT_MODE_ENABLED);
+
+        if(LIGHT_MODE_ENABLED)
+            document.body.classList.add("light_mode");
+
+        else 
+            document.body.classList.remove("light_mode");            
+        
+    }
+}
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    setLightMode(localStorage.getItem("LIGHT_MODE") == "true");    
+});
